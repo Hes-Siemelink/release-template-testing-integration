28ffd6ff575a27a56591cc4be4b55689f94fe00f
Hes Siemelink <hsiemelink@digital.ai>
2025-02-18 19:52:01 +0100

    Run release from Python test

---
+++ scratchpad/xl-test-runner.py
@@ -0,0 +1,11 @@
+import unittest
+import subprocess
+
+class TestMyTemplate(unittest.TestCase):
+
+    def test_script_success(self):
+
+        process = subprocess.run(['xl', 'apply', '-f', 'run-test-release.yaml'], capture_output=True, text=True, check=False)
+
+        self.assertEqual(process.returncode, 0, f"Script failed with exit code {process.returncode}. Output:\n{process.stdout}\n{process.stderr}")
+
