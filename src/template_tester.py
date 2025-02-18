28118fd3b1365e63b1f773d64df92ac1484a5da7
Hes Siemelink <hsiemelink@digital.ai>
2025-02-18 20:43:21 +0100

    Template tester (WIP)
    - Copies release from template and replaces tasks of a certain type with mock tasks

--- src/template_tester.py
+++ src/template_tester.py
@@ -1,13 +1,14 @@
 from digitalai.release.integration import BaseTask
+from digitalai.release.v1 import ApiClient
 from digitalai.release.v1.api.release_api import ReleaseApi, Release
 from digitalai.release.v1.api.phase_api import PhaseApi
-from digitalai.release.v1.api.task_api import TaskApi
+from digitalai.release.v1.api.task_api import TaskApi, Task
+from digitalai.release.v1.api_client import Endpoint
 
 
 class TestTemplate(BaseTask):
 
     def execute(self) -> None:
-
         # Get input properties
         template_name = self.input_properties['templateName']
         if not template_name:
@@ -26,29 +27,29 @@ class TestTemplate(BaseTask):
 
         # Get reference release
         # XXX Deserialization of the returned release fails on attachments.
-        #     Workaround is to set _check_return_type=False
+        # Workaround is to set _check_return_type=False
         reference_release = release_api.get_release(release_id, _check_return_type=False)
-
-        # allTasks = find_all_tasks(reference_release)
+        all_tasks = find_all_tasks(reference_release, mock_type)
+        task_index = 0
+        print(f"Tasks in release:\n{all_tasks}")
 
         # Copy template into this release and replace certain tasks with mocks
         for phase in base_template.phases:
             phase = convert_floats_to_ints(phase)
-            print("Adding phase:")
-            print(f"{phase}")
+
             added_phase = phase_api.add_phase(self.get_release_id(), phase=phase)
 
             for task in phase.tasks:
-                task_api.add_task_task(added_phase.id, task=task)
-
-              # if task.type.toString() == mockType:
-              #   mockTask = taskApi.newTask('xlrelease.ScriptTask')
-              #   mockTask.title = "MOCK " + task.title
-              #   original, taskIndex = find_task_by_type(allTasks, mockType, taskIndex)
-              #   mockTask.script = create_mock_script(original)
-              #   taskApi.addTask(addedPhase.id, mockTask)
-              # else:
-              #   taskApi.addTask(addedPhase.id, task)
+                if task.type == mock_type:
+                    original = all_tasks[task_index]
+                    task_index = (task_index + 1) % len(all_tasks)
+                    task_api.add_task_task(added_phase.id, task=Task(
+                        id="0",
+                        type="xlrelease.ScriptTask",
+                        title= "MOCK " + task.title,
+                        script = f"releaseVariables['output'] = '{original['output']}'"))
+                else:
+                    task_api.add_task_task(added_phase.id, task=task)
 
 
 def convert_floats_to_ints(obj):
@@ -107,64 +108,11 @@ def convert_floats_to_ints(obj):
         return obj
 
 
-def find_all_tasks(release: Release):
+def find_all_tasks(release: Release, task_type: str):
     tasks = []
     for phase in release.phases:
-        for task in phase.tasks:
-            tasks.append(task)
+        print(f"Phase: {phase}")
+        for task in phase["tasks"]:  # XXX We jump from attributes to dicts now
+            if task["type"] == task_type:
+                tasks.append(task)
     return tasks
-
-    # from com.xebialabs.xlrelease.api.v1.forms import Variable
-    #
-    #
-    # # baseTemplate = templateApi.getTemplate(releaseVariables['templateId'])
-    # baseTemplate = releaseApi.searchReleasesByTitle(releaseVariables['templateId'])[0]
-    # referenceRelease = releaseApi.getRelease(releaseVariables['releaseId'])
-    # mockType = releaseVariables['mockType']
-    #
-    # def find_all_tasks(refRel):
-    #   tasks = []
-    #   for phase in refRel.phases:
-    #     for task in phase.tasks:
-    #       tasks.append(task)
-    #   return tasks
-    #
-    # def find_task_by_type(all, type, startIndex):
-    #   for i in range(startIndex, len(all)):
-    #     if all[i].type.toString() == type:
-    #       return all[i], i + 1
-    #
-    #   raise Exception("Task not found after index %s" % startIndex)
-    #
-    # def create_mock_script(original):
-    #
-    #   # mock = original.pythonScript.getOuputProperties().toString()
-    #
-    #   # XXX Getting the output variable is hardcoded now, should be a proper lookup
-    #   return "releaseVariables['taskId'] = '%s'" %original.getProperty('xlDeployTaskId')
-    #
-    # # Reference list of previously run tasks, as a flat list for simpler lookup
-    # allTasks = find_all_tasks(referenceRelease)
-    # taskIndex = 0
-    #
-    # # Copy variables.
-    # # XXX Make this work
-    # for variable in releaseApi.getVariables(referenceRelease.id):
-    #   # print "type: %s; contents: %s" % (type(variable), variable)
-    #   formVar = Variable(variable.type.toString(), variable.key, variable.getValue(), variable.requiresValue)
-    #   releaseApi.createVariable(release.id, formVar)
-    #
-    # # Copy template into this release and replace certain tasks with mocks
-    # for phase in baseTemplate.phases:
-    #   print "PHASE" + phase.title
-    #   addedPhase = phaseApi.addPhase(release.id, phase)
-    #   for task in phase.tasks:
-    #
-    #     if task.type.toString() == mockType:
-    #       mockTask = taskApi.newTask('xlrelease.ScriptTask')
-    #       mockTask.title = "MOCK " + task.title
-    #       original, taskIndex = find_task_by_type(allTasks, mockType, taskIndex)
-    #       mockTask.script = create_mock_script(original)
-    #       taskApi.addTask(addedPhase.id, mockTask)
-    #     else:
-    #       taskApi.addTask(addedPhase.id, task)
